90e3b9f1f30729df8e12a5d77854e4b90d8bd52e
Eugene.Furmanyk <eugene.furmanyk@nordigy.ru>
2021-11-08 18:36:45 +0300

    revert - now will try it on ubuntu 20.04

--- modules/role/manifests/puppet/db.pp
+++ modules/role/manifests/puppet/db.pp
@@ -2,32 +2,9 @@ class role::puppet::db {
 
   include profile::base
 
-  # puppetdb can't detect correct version of postgresql through dfn
-  class { 'puppetdb::database::postgresql':
-    manage_package_repo => false,
-  }
-
-  # Configure puppetdb and its underlying database
   class { 'puppetdb': }
   
-  # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
-
-  # class { 'postgresql::globals':
-  #   manage_package_repo => true,
-  #   version             => '9.6',
-  # }
-
-  # class { 'postgresql::server':
-  # }
-
-  # class { 'puppetdb::database::postgresql':
-  #   listen_addresses => $::fqdn,
-  # }
-
-  # class { 'puppetdb::server':
-  #   database_host => $::fqdn,
-  # }
 
 }
 
